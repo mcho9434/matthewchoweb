c5791836de11fd770a66a79e910e21e69547d265
mcho9434 <mcho9434@usc.edu>
2022-07-17 16:33:48 -0400

    img file test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Matthew Cho's personal website
+      <img src="res/mat.jpg" alt="Matthew's face" width="500" height="500"/>
     </div>
   );
 }

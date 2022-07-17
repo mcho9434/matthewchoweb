83b26ed8aad82ec824bb009e344ec45148487afe
mcho9434 <mcho9434@usc.edu>
2022-07-17 16:43:38 -0400

    mui test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       Matthew Cho's personal website
       <img src="res/mat.jpg" alt="Matthew's face" width="500" height="500"/>
+
+      <Button variant="contained">Hello World</Button>
+
     </div>
   );
 }

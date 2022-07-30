a81efd198acf6632885cc2e2b03a53445e1f6b7a
mcho9434 <mcho9434@usc.edu>
2022-07-30 10:24:14 -0400

    simple

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Home from './pages';
+
 
 function App() {
   return (
-    <Router>
-      <Home/>
-    </Router>
+    <p>Hello</p>
   );
 }
 
